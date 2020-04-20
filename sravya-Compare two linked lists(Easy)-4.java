4357fa205606a319569863075e7b515a6e233999
CHNIJ-02527 <63992893+CHNIJ-02527@users.noreply.github.com>
2020-04-20 11:26:55 +0530

    Sravya-DS

---
+++ sravya-Compare two linked lists(Easy)-4.java
@@ -0,0 +1,36 @@
+
+
+    // Complete the compareLists function below.
+
+    /*
+     * For your reference:
+     *
+     * SinglyLinkedListNode {
+     *     int data;
+     *     SinglyLinkedListNode next;
+     * }
+     *
+     */
+    static boolean compareLists(SinglyLinkedListNode head1, SinglyLinkedListNode head2) {
+        
+       if(head1==null&&head2==null) {
+         return true;
+        }
+        SinglyLinkedListNode temp1 = head1;
+        SinglyLinkedListNode temp2 = head2;
+
+     while(temp1!=null&&temp2!=null){
+           if(temp1.data!=temp2.data){
+         return false;
+    }
+    temp1= temp1.next;
+    temp2 = temp2.next;
+   }
+    if(temp1==null&&temp2==null){
+    return true;
+   }
+    else{
+    return false;
+    }
+    }
+
