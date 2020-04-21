daedfe4603ca24f4ab51218eea8f3391d8a63c66
CHNIJ-02527 <63992893+CHNIJ-02527@users.noreply.github.com>
2020-04-21 11:27:33 +0530

    Bharghav-handson

---
+++ timeconversion(easy).java
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string s;
+    getline(cin, s);
+
+    string result = timeConversion(s);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
